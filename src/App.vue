577bc27fde172f2710a089699d1843394ce2b488
AndriyKlymash <andriyklymash2020@gmail.com>
2021-08-05 14:25:26 +0300

    lesson_1

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,80 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img v-bind:alt="imageAlt" :src="imageSrc">
+  <div class="mainBox">
+    {{ title }}
+    <p>{{ paragraph }}</p>
+    <ul>
+      <li v-for="(item, index) of items" :key="item">{{ item }}
+        <button @click="itemRemove(item, index)">remove</button>
+      </li>
+    </ul>
+
+    <h2 v-if="counter === 0">Hello</h2>
+    <h2 v-else-if="counter === 1">1</h2>
+    <h2 v-else-if="counter === 2">2</h2>
+    <h2 v-else-if="counter === 3">3</h2>
+    <h2 v-else>World</h2>
+
+    <div class="counterBox">
+      <div>
+        <Counter :counter="counter" @handleIncClick="incHandler" @handleDecClick="decHandler"/>
+      </div>
+      <div>
+        <Counter :counter="counter" @handleIncClick="incHandler" @handleDecClick="decHandler"
+                 @handleResClick="resetHandler"/>
+      </div>
+    </div>
+  </div>
+
+
 </template>
 
 <script>
+import Img from './assets/logo.png'
+import Counter from "./components/Counter";
 
 export default {
   name: 'App',
+  components: {
+    Counter
+  },
+  data() {
+    return {
+      counter: 0,
+      title: 'Dynamic text',
+      paragraph: 'This is paragraph',
+      imageAlt: 'Vue logo',
+      imageSrc: Img,
+      items: [1, 2, 3, 4]
+    }
+  },
+  methods: {
+    incHandler(a, b, c) {
+      console.log(a, b, c);
+      // console.log(this);
+      this.counter++;
+    },
 
+    decHandler() {
+      if (this.counter > 0) {
+        this.counter--;
+      }
+    },
+
+    resetHandler() {
+      this.counter = 0;
+    },
+
+    itemRemove(item, index) {
+      this.items.splice(index, 1);
+    }
+  }
 }
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.counterBox {
+  display: flex;
+  column-gap: 20px;
 }
+
 </style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="hello">
+    counter is {{ counter }}
+    <br>
+    <button :class="counter > 0 ? 'isInced': 'isDeced'" @click="inc">INC</button>
+    <button :class="{bold:true}" @click="$emit('handleDecClick')">DEC</button>
+    <button :class="counter > 0 ? 'isInced' : 'isRes'" @click="$emit('handleResClick')">Reset</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    counter: {
+      type: Number,
+      required: true
+    }
+  },
+  methods: {
+    inc() {
+      this.$emit('handleIncClick', 1, 2, 3);
+    },
+    //інший спосіб
+    // dec(){
+    //   this.$emit('handleDecClick');
+    // }
+  }
+}
+</script>
+
+<style scoped>
+.hello {
+  background: #2c3e50;
+  color: darkorange;
+}
+
+.bold {
+  font-weight: bold;
+}
+
+.isInced {
+  color: green;
+}
+
+.isDeced {
+  color: red;
+}
+
+.isRes{
+  color: grey;
+}
+</style>
+
